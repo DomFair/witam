545b213ae7e9459a8d98a80a0c3242b69686c13f
Dominic Fairman <DomFair088@gmail.com>
2021-05-30 14:11:38 -0500

    mark

---
+++ inc.db.php
@@ -0,0 +1,12 @@
+<!-- This is used to form a connection to the database. I am using PHPmyadmin for this. -->
+<?php
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PWD = '';
+    const DB = 'assessment1';   //database name
+
+    const CONNECT_MYSQL = 'mysql:host=' . HOST .
+                          ';dbname=' . DB;
+
+
+?>
